7e877d625bf8af17b20514050198d0e4597ebc4f
andrey-zub <55314972+andrey-zub@users.noreply.github.com>
2024-01-08 17:36:23 +0300

    - DI + list - scope singltone + prototype

--- src/main/java/spring_app/src/TestSpring.java
+++ src/main/java/spring_app/src/TestSpring.java
@@ -10,10 +10,24 @@ public class TestSpring {
         );
 
          //Music  music = context.getBean("musicBean", Music.class);
-         MusicPlayer plr = context.getBean("musicPlayer", MusicPlayer.class);
-         plr.player();
-         plr.playList();
+         MusicPlayer plr_1 = context.getBean("musicPlayer", MusicPlayer.class);
+         MusicPlayer plr_2 = context.getBean("musicPlayer", MusicPlayer.class);
 
+         plr_1.setVolume(40);
+        plr_1.player();
+
+        plr_2.setVolume(80);
+        plr_2.setName("Player_2");
+        plr_2.player();
+/*
+         boolean exp = plr_1 == plr_2;
+         System.out.println(exp);
+         System.out.println(plr_1);
+         System.out.println(plr_2);
+*/
+
+
+         ///
          context.close();
 
 
